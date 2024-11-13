5f30366f18f5d30c09f95df026ef19846b0d114d
Kim Minchan <galaxyeyes02@naver.com>
2024-11-13 10:05:42 +0900

    fix : add_opacity, transition

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Area from './Area';
 function App() {
   return (
     <div id='container'>
-      <Area id={"a"} url={"https://picsum.photos/id/51/600/600"} />
-      <Area id={"b"} url={"https://picsum.photos/id/11/600/600"} />
-      <Area id={"c"} url={"https://picsum.photos/id/15/600/600"} />
-      <Area id={"d"} url={"https://picsum.photos/id/20/600/600"} />
+      <Area id={"a"} url={"https://picsum.photos/id/51/800/800"} />
+      <Area id={"b"} url={"https://picsum.photos/id/11/800/800"} />
+      <Area id={"c"} url={"https://picsum.photos/id/15/800/800"} />
+      <Area id={"d"} url={"https://picsum.photos/id/20/800/800"} />
     </div>
   );
 }
