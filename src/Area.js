84a341353474b8e3e0b86a93d25923fc1c17d713
Kim Minchan <galaxyeyes02@naver.com>
2024-11-13 11:25:10 +0900

    fix : forEach->for

--- src/Area.js
+++ src/Area.js
@@ -2,11 +2,12 @@ import { useEffect } from "react"
 
 function Area({id, url}) {
     function imgSetting () {
-        const imgElement = document.querySelectorAll(`${id} img`);
-        imgElement.forEach(img => {
+        const imgElements = document.querySelectorAll(`${id} img`);
+        for (let index = 0; index < imgElements.length; index++) {
+            const img = imgElements[index];
             img.style.opacity = "0.5";
             img.style.transition = "0.3s";
-        });
+        };
     }
     function scaleAndOpacityOn(event) {
         event.target.style.transform = "scale(1.2)"
