95582ca013235dfa6190b70544e1fecce3628ca3
Kim Minchan <galaxyeyes02@naver.com>
2024-11-14 09:24:54 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import Area from './Area';
+
 
 function App() {
-  return (
-    <div id='container'>
-      <Area id={"a"} url={"https://picsum.photos/id/41/800/800"} />
-      <Area id={"b"} url={"https://picsum.photos/id/11/800/800"} />
-      <Area id={"c"} url={"https://picsum.photos/id/15/800/800"} />
-      <Area id={"d"} url={"https://picsum.photos/id/20/800/800"} />
-    </div>
-  );
+  return 
+    
+  
 }
 
 export default App;
